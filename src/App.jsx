7cc14df6a379dfd38b995f92c6efc5e6176c4d5e
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-06-08 14:07:19 -0700

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Box, Stack,Button} from '@mui/material';
+import { Box, Stack,Button,Typography} from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
@@ -35,22 +35,37 @@ function App() {
               
              }}>
               <p>hello</p>
-              <h1>I'M ERAY MUTABESHA DIEUMERCI</h1>
+              
+              <Typography variant="h3">I'M <span className='eray'>ERAY</span> MUTABESHA </Typography>
               <p>Fullstack Developer | No-code Webflow expert | Cofounder of @kVolts_lab and Tony elumelu foundation certified business manager.
                 Experienced in building modern web applications and IoT solutions</p>
                 <Stack direction="row" spacing={2}>
                   <Box><Button variant="contained">resume</Button></Box>
                   <Box  sx={{
                     padding:"10px",
-                    boxShadow:"0px 0px 1px 0px black"
+                    boxShadow:"0px 0px 1px 0px black",
+                    '&:hover': {
+                      boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
+                      cursor:"pointer"
+                    },
                   }}><FontAwesomeIcon icon={faEnvelope} /></Box>
                    <Box  sx={{
                     padding:"10px",
-                    boxShadow:"0px 0px 1px 0px black"
+                    boxShadow:"0px 0px 1px 0px black",
+                    
+                    '&:hover': {
+                      duration:"1.5s",
+                      boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
+                      cursor:"pointer"
+                    },
                   }}><FontAwesomeIcon icon={faEnvelope} /></Box>
-                   <Box  sx={{
+                  <Box  sx={{
                     padding:"10px",
-                    boxShadow:"0px 0px 1px 0px black"
+                    boxShadow:"0px 0px 1px 0px black",
+                    '&:hover': {
+                      boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
+                      cursor:"pointer"
+                    },
                   }}><FontAwesomeIcon icon={faEnvelope} /></Box>
                   
                 </Stack>
