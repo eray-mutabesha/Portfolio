5d9c76250348b28d4b9df3e191469d3f47a7cbde
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-06-09 13:52:42 -0700

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { Box, Stack,Button,Typography} from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn,faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+
 
 function App() {
 
@@ -12,7 +14,7 @@ function App() {
     
       <Box sx={{
         width:"100%",
-        height: "100vh",
+        border:"1px solid red",
         display:"grid",
         gridTemplateColumns:"10% 90%"
        
@@ -24,7 +26,7 @@ function App() {
         <Stack direction="row" spacing={10} sx={{ 
           marginLeft:"auto",
           marginRight:"auto",
-          marginTop:"100px",
+          marginTop:"150px",
           width:"80%",
           alignItems:"center"
         }}>
@@ -37,10 +39,13 @@ function App() {
               <Typography mt={2}>hello</Typography>
               
               <Typography variant="h3" mt={2}>I'M <span className='eray'>ERAY</span> MUTABESHA </Typography>
-              <Typography mt={3}>Fullstack Developer | No-code Webflow expert | Cofounder of <span className='eray'>@kVolts_lab </span>and <span className='eray'>Tony elumelu foundation</span> certified business manager.
+              <Typography mt={3} sx={{
+                color:"rgba(58, 58, 58, 0.904)"
+              }}>Fullstack Developer | No-code Webflow expert | Cofounder of <span className='eray'>@kVolts_lab </span>and <span className='eray'>Tony elumelu foundation</span> certified business manager.
                 Experienced in building modern web applications and IoT solutions</Typography>
                 <Stack direction="row" spacing={2} mt={2}>
                   <Box><Button variant="contained">resume</Button></Box>
+                  <a href="https://www.linkedin.com/in/eray-mutabesha-186240256/">
                   <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -50,7 +55,8 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faEnvelope} /></Box>
+                  }}><FontAwesomeIcon icon={faLinkedinIn } /></Box></a>
+                  <a href='https://x.com/ErayMutabesha'>
                    <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -61,7 +67,8 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faEnvelope} /></Box>
+                  }}><FontAwesomeIcon icon={faXTwitter} /></Box></a>
+                  <a href='https://github.com/eray-mutabesha'>
                   <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -71,15 +78,28 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faEnvelope} /></Box>
+                  }}><FontAwesomeIcon icon={faGithub} /></Box></a>
                   
                 </Stack>
                 
              </Box>
           
         </Stack>
+        <Box sx={{
+          marginLeft:"auto",
+          marginRight:"auto",
+          marginTop:"150px",
+          width:"90%",
+        }}>
+        <Typography variant="h5" mt={25} sx={{textAlign:"center"}}>About me</Typography>
+        <Typography mt={5} sx={{
+          color:"rgba(58, 58, 58, 0.904)"
+        }}>I am an enthusiastic web developer and entrepreneur who is passionate about innovation. I have extensive experience in full-stack development and possess skills in various frameworks/libraries such as jQuery, Laravel/InertiaJs, Codeigniter, VueJS/Nuxt, React, Bootstrap/TailwindCSS... My expertise lies in creating user-friendly interfaces and robust web applications/websites, and I have a proven track record in dynamic sectors like Fintech, IoT, and ERP. Whether I'm working independently or as part of a team, I always strive to deliver exceptional results that I can take pride in.</Typography>
         </Box>
+        </Box>
+        
       </Box>
+      
      
       </section>
   )
