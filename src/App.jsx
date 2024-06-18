34e7f833204f9339cf263ae3f04b2a4cff40d2df
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-06-18 14:38:14 -0700

    certif

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Box, Stack,Button,Typography, duration} from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { faLinkedinIn,faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -13,7 +12,9 @@ function App() {
   useEffect(() => {
     AOS.init({duration:1000}); // Initialisation de AOS
   }, []);
+
   const emailAddress = 'eraymutabesha4@gmail.com'; 
+
   return (
     
     <section>
@@ -22,11 +23,13 @@ function App() {
         width:"100%",
         display:"grid",
         gridTemplateColumns:"10% 90%"
-       
       }}>
+
+
         <Box sx={{background:"rgba(85, 85, 252)"}}>
           <h1>one</h1>
         </Box>
+
         <Box>
         <Stack direction="row" spacing={10} sx={{ 
           marginLeft:"auto",
@@ -35,22 +38,28 @@ function App() {
           width:"80%",
           alignItems:"center"
         }}>
+
              <img src='public\ErayProfil.jpg' className='profil_picture'/>
+
              <Box  data-aos="zoom-in" sx={{
               alignItems:"center",
               height:"fit-content"
-              
              }}>
+
               <Typography mt={2}>hello</Typography>
-              
               <Typography variant="h3" mt={2}>I'M <span className='eray'>ERAY</span> MUTABESHA </Typography>
               <Typography mt={3} sx={{
                 color:"rgba(58, 58, 58, 0.904)"
               }}>Fullstack Developer | No-code Webflow expert | Cofounder of <span className='eray'>@kVolts_lab </span>and <span className='eray'>Tony elumelu foundation</span> certified business manager.
                 Experienced in building modern web applications and IoT solutions</Typography>
+                
+                
                 <Stack direction="row" spacing={2} mt={2}>
+
                   <Box><Button variant="contained">resume</Button></Box>
+
                   <a href="https://www.linkedin.com/in/eray-mutabesha-186240256/">
+
                   <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -60,8 +69,14 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faLinkedinIn } /></Box></a>
+                  }}>
+                    
+                    <FontAwesomeIcon icon={faLinkedinIn } />
+                    
+                    </Box></a>
+
                   <a href='https://x.com/ErayMutabesha'>
+
                    <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -72,8 +87,14 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faXTwitter} /></Box></a>
+                  }}>
+                    
+                    <FontAwesomeIcon icon={faXTwitter} />
+                    
+                    </Box></a>
+
                   <a href='https://github.com/eray-mutabesha'>
+
                   <Box  sx={{
                     padding:"10px",
                     border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -83,23 +104,28 @@ function App() {
                       boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
                       cursor:"pointer"
                     },
-                  }}><FontAwesomeIcon icon={faGithub} /></Box></a>
+                  }}>
+                    
+                    <FontAwesomeIcon icon={faGithub} /></Box></a>
                   
                 </Stack>
                 
              </Box>
           
         </Stack>
+
         <Box data-aos="fade-up" sx={{
           marginLeft:"auto",
           marginRight:"auto",
           marginTop:"150px",
           width:"90%",
         }}>
+
         <Typography variant="h4" mt={25} >About me</Typography>
         <Typography mt={5} sx={{
           color:"rgba(92, 91, 91, 0.904)"
         }}>I am an enthusiastic web developer and entrepreneur who is passionate about innovation. I have extensive experience in full-stack development and possess skills in various frameworks/libraries such as jQuery, Laravel/InertiaJs, Codeigniter, VueJS/Nuxt, React, Bootstrap/TailwindCSS... My expertise lies in creating user-friendly interfaces and robust web applications/websites, and I have a proven track record in dynamic sectors like Fintech, IoT, and ERP. Whether I'm working independently or as part of a team, I always strive to deliver exceptional results that I can take pride in.</Typography>
+        
         </Box>
 
         <Box  sx={{
@@ -108,16 +134,17 @@ function App() {
           marginTop:"150px",
           width:"90%",
         }}>
+
         <Typography variant="h4" mt={25} >STACK & DAILY USED TOOLS</Typography>
 
         {/* skills logs */}
+
         <Box mt={5} sx={{
-          
           display:"grid",
           gridTemplateColumns:"auto auto auto auto auto auto auto auto "
         }} >
+
           <Box sx={{
-            
              border:"0.5px solid rgba(211, 211, 211, 0.671)",
              transition:"1s",
              borderRadius:"15PX",
@@ -126,15 +153,15 @@ function App() {
              textAlign:"center",
              '&:hover': {
                boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-              
              },
-           
           }}>
+            
             <img src='public\javascriptImage.png' alt='logo' className='image_skill'/>
             <Typography >javascript</Typography>
+
           </Box>
+
           <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -143,15 +170,15 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
            <img src='public\reactimage.png' alt='logo' className='image_skill'/>
-           <Typography >React js</Typography>
+           <Typography>React js</Typography>
+
          </Box>
+
          <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -160,15 +187,15 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
            <img src='public\nodejsimage.png' alt='logo' className='image_skill'/>
            <Typography >Node js</Typography>
+
          </Box>
+
          <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -177,15 +204,14 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
           
          }}>
            <img src='public\materialui.png' alt='logo' className='image_skill'/>
            <Typography >MaterialUI</Typography>
-         </Box>
+         </Box> 
+
          <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -194,13 +220,13 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
           
          }}>
            <img src='public\SQLiteimage.png' alt='logo' className='image_skill'/>
            <Typography >Sqlite</Typography>
          </Box>
+
          <Box sx={{
             
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
@@ -219,7 +245,6 @@ function App() {
            <Typography >HTML</Typography>
          </Box>
          <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -228,15 +253,15 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
            <img src='public\sassimage.png' alt='logo' className='image_skill'/>
            <Typography >Sass</Typography>
+
          </Box>
+
          <Box sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -245,15 +270,15 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
            <img src='public\git.png' alt='logo' className='image_skill'/>
            <Typography >Git</Typography>
+
          </Box>
+
          <Box mt={2} sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -262,15 +287,15 @@ function App() {
             textAlign:"center",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
-            },
-          
+            },         
          }}>
+
            <img src='public\css3image.png' alt='logo' className='image_skill'/>
            <Typography >Css</Typography>
+
          </Box>
+
          <Box mt={2} sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
@@ -278,41 +303,38 @@ function App() {
             width:"80px",
             textAlign:"center",
             '&:hover': {
-              boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
+              boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",            
             },
-          
          }}>
+
            <img src='public\figma.jpg' alt='logo' className='image_skill'/>
            <Typography >Figma</Typography>
+
          </Box>
           
-    
-         
         </Box>
         <Typography variant="h4" mt={20} >FEATURED PROJECTS</Typography>
 
         <Stack direction="row" spacing={10} mt={5} sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
             padding:"10px",
-            
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
         <Box>
         <ImageDemo />
         </Box>
+
         <Box>
          <Typography  variant="h4" className='eray'>SMS</Typography>
          <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus vitae aliquid ullam sint quae ut repellat facilis, repudiandae provident sequi quia nisi quis laborum soluta quam placeat consequuntur iusto.</Typography>
          <Button>Check it ou here </Button>
         </Box>
+
         </Stack>
         
         <Stack direction="row" spacing={10} mt={5} sx={{
@@ -321,49 +343,45 @@ function App() {
             transition:"1s",
             borderRadius:"15PX",
             padding:"10px",
-            
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
         <Box>
         <ImageDemo />
         </Box>
+
         <Box>
          <Typography  variant="h4" className='eray'>SMS</Typography>
          <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus vitae aliquid ullam sint quae ut repellat facilis, repudiandae provident sequi quia nisi quis laborum soluta quam placeat consequuntur iusto.</Typography>
          <Button>Check it ou here </Button>
         </Box>
-        </Stack>
 
-        
+        </Stack>
+   
         <Stack direction="row" spacing={10} mt={5} sx={{
-            
             border:"0.5px solid rgba(211, 211, 211, 0.671)",
             transition:"1s",
             borderRadius:"15PX",
             padding:"10px",
-            
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
-          
          }}>
+
         <Box>
         <ImageDemo />
         </Box>
+
         <Box>
          <Typography  variant="h4" className='eray'>SMS</Typography>
          <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus vitae aliquid ullam sint quae ut repellat facilis, repudiandae provident sequi quia nisi quis laborum soluta quam placeat consequuntur iusto.</Typography>
          <Button>Check it ou here </Button>
         </Box>
+
         </Stack>
         
-        
-
         <Typography variant="h4" mt={20} >CONTACT</Typography>
         <Typography variant="h5" mt={5} sx={{color:"rgba(92, 91, 91, 0.678)"}}>Get in Touch</Typography>
 
@@ -371,6 +389,7 @@ function App() {
           display:"flex",
           justifyContent:"space-between"
         }}>
+
           <Box sx={{border:"0.5px solid rgba(211, 211, 211, 0.671)",
             borderRadius:"5PX",
             width:"45%",
@@ -380,13 +399,13 @@ function App() {
             transition:"1s",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
            }}>
 
          <img src='public\whatshapp-removebg-preview.png' alt='email' className='image_wtp_contact'/>
-  <a href='https://wa.me/+250791434737?text=Hello%2C%20I%20am%20interested%20in%20your%20services'><Typography>+250 791434737</Typography></a>
+         <a href='https://wa.me/+250791434737?text=Hello%2C%20I%20am%20interested%20in%20your%20services'><Typography>+250 791434737</Typography></a>
          <Typography sx={{ color:"rgba(92, 91, 91, 0.678)"}}>Let's chat on whatsApp</Typography>
+          
           </Box>
 
           <Box sx={{
@@ -399,7 +418,6 @@ function App() {
             transition:"1s",
             '&:hover': {
               boxShadow:"0px 0px 15px 0px rgb(177, 175, 175)",
-             
             },
             }}>
 
@@ -412,9 +430,65 @@ function App() {
         </Box>
 
         <Typography variant="h4" mt={20} >CONTACT</Typography>
-        </Box>
-        </Box>
+
+        <Stack direction="row" spacing={10}>
+
+        <Box sx={{
+            border:"0.5px solid rgba(211, 211, 211, 0.671)",
+            borderRadius:"5PX",
+            width:"150px",
+            textAlign:"center",
+            display:"grid",
+            gap:"30px"
+            }}>
+              
+              <Box>
+              <img src='public\images.png' alt='image' className='freecodecamp_image'/>
+              <Typography>Web developement</Typography>
+              </Box>
+              <Box sx={{
+                background:"rgb(182, 182, 182)",
+                width:"100%",
+                
+              }}>
+              <Typography sx={{
+                borderTop:"0.5px solid rgba(211, 211, 211, 0.671)",
+                color:"rgba(92, 91, 91, 0.678)"}}>provide by freecodecamp</Typography>
+              </Box>
+              
+            </Box>
+
+            <Box sx={{
+            border:"0.5px solid rgba(211, 211, 211, 0.671)",
+            borderRadius:"5PX",
+            width:"150px",
+            textAlign:"center",
+            display:"grid",
+            gap:"30px"
+            }}>
+              
+              <Box>
+              <img src='public\images.png' alt='image' className='freecodecamp_image'/>
+              <Typography>JS algorithms and...</Typography>
+              </Box>
+              <Box sx={{
+                background:"rgb(182, 182, 182)",
+                width:"100%",
+                
+              }}>
+              <Typography sx={{
+                borderTop:"0.5px solid rgba(211, 211, 211, 0.671)",
+                color:"rgba(92, 91, 91, 0.678)"}}>provide by freecodecamp</Typography>
+              </Box>
+              
+            </Box>
+        </Stack>
         
+
+            <Typography variant="h4" mt={20} >CONTACT</Typography>
+
+        </Box>
+        </Box>
       </Box>
       
      
